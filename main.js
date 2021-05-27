3d680d14eda49e754f16618c8eff04d7f6341179
Aarav9797 <71883558+Aarav9797@users.noreply.github.com>
2021-05-27 16:59:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,17 +8,17 @@ canvas.center()
 }
 function draw(){
     image(img,0,0,700,500)
-    fill("red")
-    textSize(20)
-    text("dog",300,100)
-    fill("red")
-    textSize(20)
-    text("Cat",400,150)
-    stroke("red")
-    noFill()
-    rect(350,70,-250,400)
-    rect(600,70,-250,400)
-    
+
+    if(status!=""){
+        for(I=0;I<object.length;I++){
+            fill("red")
+            textSize(20)
+            text(object[I].label,object[I].x,object[I].y)
+            stroke("red")
+            noFill()
+            rect(object[I].x,object[I].y,object[I].width,object[I].height)
+        }
+    }
 }
 
 function preload(){
@@ -35,5 +35,6 @@ function gotresults(error,result){
     }
     else{
         console.log(result)
+        object=result
     }
 }
